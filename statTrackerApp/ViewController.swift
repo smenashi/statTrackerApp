d04ef8da2f946126d364ac7d35f5747e1eec9860
Sophie Menashi <smenashi@hamilton.edu>
2018-09-11 15:33:36 -0400

    basic app (with add 1 and count display) implemented

--- statTrackerApp/ViewController.swift
+++ statTrackerApp/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    var countStr = ""
 
+    @IBOutlet weak var displayCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func addPressed(_ sender: Any) {
+        count += 1
+    }
+    
+    
+    @IBAction func getCountPressed(_ sender: Any) {
+        countStr = String(count)
+        displayCount.text = countStr
+    }
+    
+    
+    /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+     */
 
 }
 
