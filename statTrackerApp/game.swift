3aa8617bfa655f3d99ac89805e24f93fc8a2965d
Sophie Menashi <smenashi@hamilton.edu>
2018-09-23 13:54:31 -0400

    added player and game class

---
+++ statTrackerApp/game.swift
@@ -0,0 +1,43 @@
+//
+//  game.swift
+//  statTrackerApp
+//
+//  Created by Sophie Menashi on 9/23/18.
+//  Copyright © 2018 Sophie Menashi. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    var _season = "2018"
+    var _gameId = 0
+    var lineUp = [Player] ()
+    
+    //hard-coding game
+    init(player1:Player, player2:Player, player3:Player, player4:Player, player5:Player, player6:Player, player7:Player, player8:Player, player9:Player, player10:Player, player11:Player, player12:Player, player13:Player, player14:Player, player15:Player, player16:Player, player17:Player, player18:Player, player19:Player, player20:Player, player21:Player, player22:Player){
+        lineUp.append(player1)
+        lineUp.append(player2)
+        lineUp.append(player3)
+        lineUp.append(player4)
+        lineUp.append(player5)
+        lineUp.append(player6)
+        lineUp.append(player7)
+        lineUp.append(player8)
+        lineUp.append(player9)
+        lineUp.append(player10)
+        lineUp.append(player11)
+        lineUp.append(player12)
+        lineUp.append(player13)
+        lineUp.append(player14)
+        lineUp.append(player15)
+        lineUp.append(player16)
+        lineUp.append(player17)
+        lineUp.append(player18)
+        lineUp.append(player19)
+        lineUp.append(player20)
+        lineUp.append(player21)
+        lineUp.append(player22)
+    }
+    
+    
+}
