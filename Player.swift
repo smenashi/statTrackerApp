2871ac97a228bf97779ce16bd32421d0d2369d18
Sophie Menashi <smenashi@hamilton.edu>
2018-09-24 14:47:49 -0400

    renaming...

---
+++ Player.swift
@@ -0,0 +1,99 @@
+//
+//  Player.swift
+//  statTrackerApp
+//
+//  Created by Sophie Menashi on 9/21/18.
+//  Copyright © 2018 Sophie Menashi. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+class Player {
+    
+    //funcitonal attributes
+    var enabled = false
+    // var clock
+    
+    // hard-coding these attributes for now
+    var _firstName = "John"
+    var _lastName = "Doe"
+    var _jerseyNumber = 0
+    
+    // stat variables
+    var shotFor = 0
+    var shotAgainst = 0
+    var goalFor = 0
+    var goalAgainst = 0
+    var penalty = 0
+    
+    /*
+     // database attributes
+     let firstName = Expression<String?>("firstName")
+     let lastName = Expression<String?>("lastName")
+     let jerseyNumber = Expression<Int64?>("jerseyNumber")
+     
+     */
+    
+    init(firstName:String, lastName:String, jerseyNum:Int) {
+        _firstName = firstName
+        _lastName = lastName
+        _jerseyNumber = jerseyNum
+    }
+    
+    // enabling/disabling
+    func enablePlayer() {
+        enabled = true
+    }
+    
+    func disablePlayer() {
+        enabled = false
+    }
+    
+    func isEnabled() -> Bool {
+        return enabled
+    }
+    
+    // stat methods
+    func increaseShotFor() {
+        shotFor += 1
+    }
+    
+    func increaseShotAgainst() {
+        shotAgainst += 1
+    }
+    
+    func increaseGoalFor() {
+        goalFor += 1
+    }
+    
+    func increaseGoalAgainst() {
+        goalFor += 1
+    }
+    
+    func increasePenalty() {
+        penalty += 1
+    }
+    
+}
+
+extension Player: Equatable {
+    static func == (lhs: Player, rhs: Player) -> Bool {
+        return lhs.enabled == rhs.enabled &&
+            lhs._lastName == rhs._lastName &&
+            lhs._firstName == rhs._firstName &&
+            lhs._jerseyNumber == rhs._jerseyNumber &&
+            lhs.shotFor == rhs.shotFor &&
+            lhs.shotAgainst == rhs.shotAgainst &&
+            lhs.goalFor == rhs.goalFor &&
+            lhs.goalAgainst == rhs.goalAgainst &&
+            lhs.penalty == rhs.penalty
+    }
+}
+
+extension Player: Hashable {
+    var hashValue: Int {
+        return enabled.hashValue ^ _lastName.hashValue ^ _firstName.hashValue ^ _jerseyNumber.hashValue ^ shotFor.hashValue ^ shotAgainst.hashValue ^ goalFor.hashValue ^ goalAgainst.hashValue ^ penalty.hashValue
+    }
+}
+
--- statTrackerApp/game.swift
+++ statTrackerApp/game.swift
@@ -1,5 +1,5 @@
 //
-//  game.swift
+//  Game.swift
 //  statTrackerApp
 //
 //  Created by Sophie Menashi on 9/23/18.
@@ -11,33 +11,107 @@ import Foundation
 class Game {
     var _season = "2018"
     var _gameId = 0
-    var lineUp = [Player] ()
+    var onIce = Set<Player>()
     
-    //hard-coding game
+    // players
+    var _player1:Player
+    var _player2:Player
+    var _player3:Player
+    var _player4:Player
+    var _player5:Player
+    var _player6:Player
+    var _player7:Player
+    var _player8:Player
+    var _player9:Player
+    var _player10:Player
+    var _player11:Player
+    var _player12:Player
+    var _player13:Player
+    var _player14:Player
+    var _player15:Player
+    var _player16:Player
+    var _player17:Player
+    var _player18:Player
+    var _player19:Player
+    var _player20:Player
+    var _player21:Player
+    var _player22:Player
+    
+    // hard-coding game
     init(player1:Player, player2:Player, player3:Player, player4:Player, player5:Player, player6:Player, player7:Player, player8:Player, player9:Player, player10:Player, player11:Player, player12:Player, player13:Player, player14:Player, player15:Player, player16:Player, player17:Player, player18:Player, player19:Player, player20:Player, player21:Player, player22:Player){
-        lineUp.append(player1)
-        lineUp.append(player2)
-        lineUp.append(player3)
-        lineUp.append(player4)
-        lineUp.append(player5)
-        lineUp.append(player6)
-        lineUp.append(player7)
-        lineUp.append(player8)
-        lineUp.append(player9)
-        lineUp.append(player10)
-        lineUp.append(player11)
-        lineUp.append(player12)
-        lineUp.append(player13)
-        lineUp.append(player14)
-        lineUp.append(player15)
-        lineUp.append(player16)
-        lineUp.append(player17)
-        lineUp.append(player18)
-        lineUp.append(player19)
-        lineUp.append(player20)
-        lineUp.append(player21)
-        lineUp.append(player22)
+        _player1 = player1
+        _player2 = player2
+        _player3 = player3
+        _player4 = player4
+        _player5 = player5
+        _player6 = player6
+        _player7 = player7
+        _player8 = player8
+        _player9 = player9
+        _player10 = player10
+        _player11 = player11
+        _player12 = player12
+        _player13 = player13
+        _player14 = player14
+        _player15 = player15
+        _player16 = player16
+        _player17 = player17
+        _player18 = player18
+        _player19 = player19
+        _player20 = player20
+        _player21 = player21
+        _player22 = player22
     }
     
     
+    // managing ice
+    func iceIsFull() -> Bool {
+        return onIce.count == 5
+    }
+    
+    func putOnIce(addPlayer:Player) {
+        if !iceIsFull() {
+            onIce.insert(addPlayer)
+            addPlayer.enablePlayer()
+        }
+    }
+    
+    func takeOffIce(removePlayer:Player) {
+        onIce.remove(removePlayer)
+        removePlayer.disablePlayer()
+    }
+    
+    
+    // stat buttons
+    func shotFor() {
+        for player in onIce {
+            player.increaseShotFor()
+        }
+    }
+    
+    func shotAgainst() {
+        for player in onIce {
+            player.increaseShotAgainst()
+        }
+    }
+    
+    func goalFor() {
+        for player in onIce {
+            player.increaseGoalFor()
+        }
+    }
+    
+    func goalAgainst() {
+        for player in onIce {
+            player.increaseGoalAgainst()
+        }
+    }
+    
+    func penalty(player:Player) {
+        if player.isEnabled() {
+            player.increasePenalty()
+        }
+    }
+    
 }
+
