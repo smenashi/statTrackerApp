0433f83aefc4a01c1dd4e1f0f311c59650ce3dc8
Nikoloz (Nick) Chkonia <nchkonia@mac-21965.local>
2018-10-25 14:24:20 -0400

    - Added multiple storyboards (Roster Select Screen, Main Menu) with their respective viewControllers - Added buttons that move between these views - Added Table View on LHS of Roster Select Screen - Added copy of Player Buttons to RHS of Roster Select Screen

---
+++ statTrackerApp/MainMenuViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainMenuViewController.swift
+//  statTrackerApp
+//
+//  Created by Nikoloz (Nick) Chkonia on 10/25/18.
+//  Copyright © 2018 Sophie Menashi. All rights reserved.
+//
+
+import UIKit
+
+// NOTE: this is tied to the Main Menu Screen in HockeyUI.storyboard
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ statTrackerApp/RosterScreenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RosterScreenViewController.swift
+//  statTrackerApp
+//
+//  Created by Nikoloz (Nick) Chkonia on 10/25/18.
+//  Copyright © 2018 Sophie Menashi. All rights reserved.
+//
+
+import UIKit
+
+// NOTE: this is tied to the Roster Screen in HockeyUI.storyboard
+class RosterScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
